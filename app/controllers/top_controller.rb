2115e673a423509939d7f40d9ec86bcdc42b2c60
masaki925 <masaki925@gmail.com>
2012-07-09 04:38:50 +0900

    微調整

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -10,7 +10,8 @@ class TopController < ApplicationController
       @user_meetups.each do |meetup|
         meetup.users.each do |user|
           next if user == current_user
-          review = UserReview.find(:all, conditions: ["user_id = ? AND meetup_id = ? AND reviewed_user_id = ?", current_user.id, meetup.id, user.id])
+          #review = UserReview.find(:all, conditions: ["user_id = ? AND meetup_id = ? AND reviewed_user_id = ?", current_user.id, meetup.id, user.id])
+          review = UserReview.find(:all, conditions: ["user_id = ? AND reviewed_user_id = ?", current_user.id, user.id])
           if review.blank?
             @review_to_write << [meetup.id, user.id, user.name]
           end
