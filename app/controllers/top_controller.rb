eb6d9eb0e71030d524a6a7b5d683180c5b7d5db8
masaki925 <masaki925@gmail.com>
2012-07-22 11:49:27 +0900

    filter meetup "except me" at top page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,19 +1,36 @@
 class TopController < ApplicationController
   def index
     if @user = current_user
-      @recent_meetups = Meetup.all(limit: 3, order: 'id DESC')
-      @user_avails = @user.user_avails 
+      @user_avails  = @user.user_avails 
       @user_meetups = @user.meetups
 
-      # 書くべきreview のリスト
+      # pick up meetups except me
+      @recent_meetups_exceptme  = Array.new
+      offset = 0
+      loop do
+        break if @recent_meetups_exceptme.size >= 3
+        recent_meetup_candidates = Meetup.all(limit: 10, order: 'id DESC', offset: offset)
+        break if recent_meetup_candidates.size <= 0
+
+        recent_meetup_candidates -= @user_meetups
+        recent_meetup_candidates.each_with_index do |m_cand, idx|
+          break if idx >= 3
+          @recent_meetups_exceptme << m_cand
+        end
+        offset += 10
+      end
+
+      # Review to write
+      ### FIXME: performance problem will occurs
       @review_to_write = Array.new
+      check_list       = Array.new
       @user_meetups.each do |meetup|
         meetup.users.each do |user|
           next if user == current_user
-          #review = UserReview.find(:all, conditions: ["user_id = ? AND meetup_id = ? AND reviewed_user_id = ?", current_user.id, meetup.id, user.id])
           review = UserReview.find(:all, conditions: ["user_id = ? AND reviewed_user_id = ?", current_user.id, user.id])
-          if review.blank?
+          if review.blank? && ! check_list.include?(user.id)
             @review_to_write << [meetup.id, user.id, user.name]
+            check_list << user.id
           end
         end
       end
